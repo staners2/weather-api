0e261910460e534cc54272d2b9464cc0bfe20c81
Dmitriy Aratin <dima.aratin0@gmail.com>
2022-01-13 13:57:35 +0700

    append url external api

--- API/constant/API.py
+++ API/constant/API.py
@@ -1,4 +1,4 @@
 
 class API(object):
-    GENERATE_URL = "http://numbersapi.com/{}/{}"
+    GENERATE_URL = "https://api.weatherbit.io/v2.0/current?city={}&country={}&lang={}&key={}"
     API_KEY = "7979ce46880a4d32867818e45019d006"
