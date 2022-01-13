3afe3840e698186baee0e1f3da037ecc3ba4f59a
Dmitriy Aratin <dima.aratin0@gmail.com>
2022-01-13 13:56:47 +0700

    append userprofile in admin panel

--- API/admin.py
+++ API/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Language, Cities
+from .models import Language, Cities, UserProfile
 
 admin.site.register(Language)
 admin.site.register(Cities)
+admin.site.register(UserProfile)
 
 # Register your models here.
